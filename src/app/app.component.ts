175b02eaecd6d26a3a1c6cad2029e63701ebeb74
bnoa-io <brayanapeles.dev@gmail.com>
2023-12-10 04:20:08 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PostsComponent } from './pages/posts/posts.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    PostsComponent
+  ],
+  template: `
+    <app-posts></app-posts>
+    <router-outlet></router-outlet>
+  `,
 })
+
 export class AppComponent {
   title = 'angular-desafio-codelandia-01';
 }
